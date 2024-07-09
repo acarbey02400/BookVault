220a84fa8f4ff5343dff3bdf485ba54355993e50
acaribrahim <ibrahim.acar@cognitron.com.tr>
2024-07-09 11:42:52 +0300

    add validator to user

---
+++ Application/Features/Users/Commands/UpdateUser/UpdateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Users.Commands.UpdateUser
+{
+    public class UpdateUserValidator:AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(p=>p.FirstName).NotEmpty().MinimumLength(3);
+            RuleFor(p=>p.LastName).NotEmpty().MinimumLength(2);
+            RuleFor(p=>p.Id).NotEmpty();
+        }
+    }
+}
